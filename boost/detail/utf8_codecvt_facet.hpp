c4097604fa275e0280cf829a7649f75cdeccc525
Richard Bateman <taxilian@gmail.com>
2010-11-24 16:45:17 -0700

    Added missing uuid lib

--- boost/detail/utf8_codecvt_facet.hpp
+++ boost/detail/utf8_codecvt_facet.hpp
@@ -103,7 +103,7 @@ namespace std {
 
 BOOST_UTF8_BEGIN_NAMESPACE
 
-struct utf8_codecvt_facet :
+struct BOOST_UTF8_DECL utf8_codecvt_facet :
     public std::codecvt<wchar_t, char, std::mbstate_t>  
 {
 public:
